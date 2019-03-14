2ef74fb42dc8a774b18b71008c57bc685919ef9d
Roma Teleshyk <teleshyk.r@gmail.com>
2019-03-14 22:52:04 +0200

    Creates Spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Spinner.scss';
+
+const Spinner = () => {
+	return (
+		<div className="spinner">
+			<div className="line"/>
+			<div className="line"/>
+			<div className="line"/>
+		</div>
+	);
+};
+
+export default Spinner;
