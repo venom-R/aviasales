8dde46b59aaacda8fd39c7bd3f270ee5981f5800
Roma Teleshyk <teleshyk.r@gmail.com>
2019-03-17 22:48:04 +0200

    Changes class component App to function component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row } from 'react-bootstrap';
 import Header from '../Header';
 import Main from '../Main';
 import Sidebar from '../Sidebar';
 import TicketList from '../../containers/TicketListContainer';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Header/>
-				<Main>
-					<Row>
-						<Sidebar/>
-						<TicketList/>
-					</Row>
-				</Main>
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div className="App">
+			<Header/>
+			<Main>
+				<Row>
+					<Sidebar/>
+					<TicketList/>
+				</Row>
+			</Main>
+		</div>
+	);
+};
 
 export default App;
