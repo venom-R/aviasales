4c478a24401285bf03a299d3893f420b03bbf17b
Roma Teleshyk <teleshyk.r@gmail.com>
2019-03-22 21:50:32 +0200

    Updates

---
+++ precache-manifest.0a52f9cb11887a95089c4a1ecb8a5d6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad852b754398b29fe3199d9b3859bd39",
+    "url": "/aviasales/static/media/plane.ad852b75.svg"
+  },
+  {
+    "revision": "a200b56511f2587f4f2b9e16299a96df",
+    "url": "/aviasales/static/media/logo.a200b565.svg"
+  },
+  {
+    "revision": "c7d0d0b982dad573af27c6ca6bbb859e",
+    "url": "/aviasales/static/media/checkbox.c7d0d0b9.svg"
+  },
+  {
+    "revision": "907e94a2560f39798db9",
+    "url": "/aviasales/static/js/runtime~main.907e94a2.js"
+  },
+  {
+    "revision": "8c73d9eeb2643852e626",
+    "url": "/aviasales/static/js/main.8c73d9ee.chunk.js"
+  },
+  {
+    "revision": "be05ffc171c54a977f4a",
+    "url": "/aviasales/static/js/2.be05ffc1.chunk.js"
+  },
+  {
+    "revision": "8c73d9eeb2643852e626",
+    "url": "/aviasales/static/css/main.ca209100.chunk.css"
+  },
+  {
+    "revision": "8a28d4e180aa6e84c351cbd542ab07a6",
+    "url": "/aviasales/index.html"
+  }
+];
